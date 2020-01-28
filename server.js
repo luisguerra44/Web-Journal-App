c78600bc9f55c763c4a619223a3afe9c43e3f010
luisguerra44 <49825068+luisguerra44@users.noreply.github.com>
2020-01-28 00:29:06 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Callback function to complete GET '/all'
+app.get('/all', sendData);
+
+function sendData(req, res) {
+  res.send(projectData);
+};
+// Post Route
+app.post('/add', addInfo);
+
+function addInfo(req, res) {
+  projectData['date'] = req.body.date;
+  projectData['temp'] = req.body.temp;
+  projectData['content'] = req.body.content;
+  res.send(projectData);
+}
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, function () {
+    console.log(`running on localhost: ${port}`);
+});
